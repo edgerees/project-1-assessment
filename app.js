7f06b878f9d9ad168d47d16e01669b91be687984
Edree Huss <edreeshuss@gmail.com>
2020-11-12 12:23:39 -0500

    [Unit1]Complete unit 1 Assesment.

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    holder.innerHTML= value;
+}
+
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+    holder.innerHTML= value;
+}
+
+
+
+
+
+
